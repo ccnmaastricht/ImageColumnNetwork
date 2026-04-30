110fbe3783c8d336feec0d2dcb5b50d62c08715c
dasjadeleeuw <dasjadeleeuw@hotmail.com>
2026-04-30 16:14:49 +0200

    device device

--- scripts/test_cluster.py
+++ scripts/test_cluster.py
@@ -2,7 +2,8 @@ print('Hello world!')
 
 import torch
 
-test_tensor = torch.tensor([0., 1., 0.])
+device = "cuda" if torch.cuda.is_available() else "cpu"
+test_tensor = torch.tensor([0., 1., 0.], device=device)
 
 print(test_tensor)
 print(test_tensor.device)
